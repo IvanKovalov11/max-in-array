044bcbb367fa20d624c3d6da8b3396b892b98512
IvanKovalov11 <32193366+IvanKovalov11@users.noreply.github.com>
2017-09-29 18:32:26 +0300

    Update max-in-array2.cpp

--- max-in-array2.cpp
+++ max-in-array2.cpp
@@ -1,3 +1,6 @@
+// ConsoleApplication2.cpp: определяет точку входа для консольного приложения.
+//
+
 #include "stdafx.h"
 #include <iostream>
 #include <sstream>
@@ -23,22 +26,35 @@ int main()
 {
 	int a[10];
 	int b[10];
-	int max = -2147483647;
+	int max1 = -2147483647;
+	int max2 = -2147483647;
+	int maxsum = -2147483647;
 
-	if (read_numbers(a) && read_numbers(b)) {
-		for (int i = 0; i < 10; ++i) {
-			for (int j = 0; j < 10; ++j){
-				if (i <= j) {
-					if (a[i] + b[j] > max)
-						max = a[i] + b[j];
-							}
-						}
-			 
-					}
-		cout << "max=" << max;
-				}
-	else {
+	if (read_numbers(a) && read_numbers(b))
+	{
+		{
+		for (int i = 0; i < 10; ++i)
+		{
+			if (a[i] > max1)
+			{
+				max1 = a[i];
+			}
+		}
+		for (int j = 0; j < 10; ++j)
+		{
+			if (b[j] > max2)
+			{
+				max2 = b[j];
+			}
+		}
+		maxsum = max1 + max2;
+	}
+	cout << "maxsum=" << maxsum;
+}
+	else 
+	{
 		cout << "An error has occurred while reading numbers";
 	}
 	system("pause");
 }
+
