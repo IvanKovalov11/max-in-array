8b2e5abbbb1f7654c73dc5781ba0ffdd6997306e
IvanKovalov11 <32193366+IvanKovalov11@users.noreply.github.com>
2017-09-29 12:15:02 +0300

    Create max-in-array1.cpp

---
+++ max-in-array1.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	for (string string; getline(cin, string); ) {
+		int nmb[10];
+		istringstream stream(string);
+		bool failure = false;
+		for (int i = 0; i < 10; ++i) {
+			if (!(stream >> nmb[i])) {
+				failure = true;
+				break;
+			}
+		}
+
+		int max = -65000;
+
+		if (!failure) {
+			for (int i = 0; i < 10; ++i) {
+				if (nmb[i] > max) {
+					max = nmb[i];
+				}				
+			}
+			cout << max << endl;
+		}
+		else {
+			cout << "An error has occured while reading numbers from line" << std::endl;
+		}
+	}
+}
